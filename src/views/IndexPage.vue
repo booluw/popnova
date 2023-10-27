9bf5919a392626276bd5d0bad87fb2db3d6e4b90
= <=>
2023-10-27 18:54:07 +0100

    :tada: some fixes and section completion

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -1,17 +1,23 @@
 <script lang="js" setup>
-import { onMounted, ref } from 'vue';
+import { nextTick, onMounted, ref } from 'vue';
+
 import gsap from "gsap";
 import Lenis from "@studio-freight/lenis";
-
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-const jumboText = ref(null);
-const lenis = new Lenis();
+import { useMouse } from '../composables/useMouse';
 
-onMounted(() => {
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+const lenis = new Lenis();
+const container = ref(null);
+let ctx;
+const { x, y } = useMouse();
+
+onMounted(async () => {
+  await nextTick();
   function raf(time) {
     lenis.raf(time)
     requestAnimationFrame(raf)
@@ -19,42 +25,62 @@ onMounted(() => {
 
   requestAnimationFrame(raf)
 
-  const tl = gsap.timeline().to(".banner", {
-    opacity: 1,
-    duration: 2.5,
-    ease: "slow(0.5,0.5,false)"
-  });
+  ctx = gsap.context((self) => {
+    const sections = gsap.utils.toArray(".section");
+    gsap.from("div.uppercase", {
+      translateY: -10,
+      opacity: 0.4,
+      duration: .5,
+      ease: 'ease.in'
+    })
+    const mainTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "body",
+        start: "clamp(top center)",
+        end: "clamp(end -1000px)",
+        scrub: true,
+        pin: true
+      },
+    })
 
-  const mainTl = gsap.timeline({
-    scrollTrigger: {
-      trigger: "body",
-      start: "clamp(top center)",
-      end: "clamp(end -1000px)",
-      scrub: true,
-      pin: true
-    },
-  })
+    mainTl.to(".jumbo h1", {
+      scale: 1000,
+      x: "-1000vw",
+      duration: 5,
+      ease: "slow(0.1,0.1,false)"
+    })
 
-  mainTl.to(".jumbo h1", {
-    scale: 1000,
-    x: "-1000vw",
-    duration: 5,
-    ease: "slow(0.1,0.1,false)"
-  })
+    const tl = gsap.timeline().to(".banner", {
+      opacity: 1,
+      duration: .5,
+      ease: "slow(0.5,0.5,false)"
+    });
 
-  mainTl.add(tl, .05);
-  mainTl
-    .add(gsap.to(".banner__heading", {
+    mainTl.add(tl, .05);
+    mainTl.add(gsap.to(".banner__heading", {
       translateY: 0,
       opacity: 1
     }), .1);
-    
+
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        pin: '.wrapper',
+        scrub: 1,
+        snap: true,
+      }
+    })
+  }, container.value)
+
 })
 </script>
 <template>
-  <section class="overflow-x-hidden">
+  <div class="absolute p-4 border-secondary border text-white z-[90] rounded-full mouse" :style="`top: ${y - 20}px; left: ${x - 20}px`">
+    <div class="p-1 border-secondary border bg-secondary rounded-full" />
+  </div>
+  <section class="overflow-x-hidden" ref="container">
     <section
-      id="tagline"
       class="jumbo min-h-screen overflow-hidden flex flex-col justify-center items-center bg-primary"
     >
       <div class="uppercase text-center">
@@ -64,8 +90,6 @@ onMounted(() => {
           Welcome to
         </h2>
         <h1
-          ref="jumboText"
-          id="jumboText"
           class="font-bold font-serif text-6xl md:text-[100px] overflow-hidden text-white"
         >
           popnova
@@ -73,8 +97,9 @@ onMounted(() => {
       </div>
     </section>
     <section class="content">
+      <!-- <img src="@/assets/imgs/popnova/Model-with-facial-paint.jpg" class="w-64 h-auto absolute z-[90] opacity-75" :style="`top: ${y - 20}px; left: ${x - 20}px`" /> -->
       <section
-        class="min-h-screen overflow-hidden bg-white -mt-[100vh] opacity-0 relative p-20 flex gap-20 justify-between items-center section banner"
+        class="h-[200vh] overflow-hidden bg-white -mt-[100vh] opacity-0 relative p-20 flex gap-20 justify-between items-center banner"
       >
         <img
           src="@/assets/imgs/popnova/Model-with-white-hair.jpg"
@@ -103,26 +128,66 @@ onMounted(() => {
             </h1>
             <div class="md:-mt-[50px] md:w-5/6">
               <span class="text-2xl bg-white bg-opacity-50 px-2">
-                Join us on this remarkable summer journey and let your wardrobe reflect the beauty of the season. Be prepared to captivate and enchant wherever you go. It's a summer like no other, and you deserve nothing less than the finest from Popnova
+                Join us on this remarkable summer journey and let your wardrobe
+                reflect the beauty of the season. Be prepared to captivate and
+                enchant wherever you go. It's a summer like no other, and you
+                deserve nothing less than the finest from Popnova
               </span>
             </div>
           </div>
         </div>
       </section>
-      <section class="h-screen bg-red-400 section"></section>
-      <section class="h-screen bg-green-400 section"></section>
-      <section class="h-screen bg-primary section"></section>
-      <section class="h-screen bg-secondary section"></section>
     </section>
+    <div class="wrapper overflow-hidden relative min-h-screen">
+      <section class="cont flex w-[300vw]">
+        <section class="h-[100vh] min-w-[100vw] bg-green-500 flex justify-between items-center px-28 relative section">
+          <div>
+            <h2 class="font-bold font-serif text-8xl text-white capitalize tracking-wider">
+              The future of African fashion
+            </h2>
+            <p class="text-3xl mt-5 text-white opacity-75">
+              Popnova is <span class="text-primary">reimagining</span> how Africa's fashion is represented.
+            </p>
+          </div>
+        </section>
+        <section class="h-[100vh] min-w-[100vw] bg-primary section"></section>
+        <section class="h-[100vh] min-w-[100vw] bg-white section"></section>
+        <section class="h-[100vh] min-w-[100vw] bg-secondary section"></section>
+      </section>
+    </div>
+    <section class="bg-primary h-screen">Hello Worl</section>
   </section>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .banner {
   &__heading {
-    background: -webkit-linear-gradient(rgba(#d2bb98, 5), rgba(#a54132, .75));
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+    background: -webkit-linear-gradient(rgba(#d2bb98, 5), rgba(#a54132, 0.75));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+
+.mouse {
+  box-shadow: 0 0 0 0 darken(rgba(#d2bb98, 1), 50%);
+  transform: scale(1);
+  animation: pulse 2s infinite ease-in-out;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(.95);
+    box-shadow: 0 0 0 0 rdarken(gba(#d2bb98, .7), 50%);
+  }
+
+  70% {
+    transform: scale(1);
+    box-shadow: 0 0 0 10px darken(rgba(#d2bb98, 0), 50%);
+  }
+
+  100% {
+    transform: scale(.95);
+    box-shadow: 0 0 0 0 darken(rgba(#d2bb98, 0), 50%);
   }
 }
 </style>
